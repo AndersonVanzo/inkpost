4e2ebf12388e89d33036beb20c1c1960aeb13569
AndersonVanzo <anderson.vanzo01@gmail.com>
2026-09-23 00:42:08 -0300

    feat: reading url

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(name = "inkpost")]
+#[command(version = "0.1.0")]
+#[command(about = "save DEV Community posts to your Kidle")]
+struct Cli {
+    #[arg(long)]
+    url: String,
+}
+
+struct Params {
+    username: String,
+    slug: String,
+}
+
+fn parse_params(url: String) -> Params {
+    let normalized_url = url.replace("https://", "");
+    if !normalized_url.starts_with("dev.to/") {
+        panic!("URL must be from dev.to");
+    }
+
+    let parts: Vec<&str> = normalized_url.split("/").collect();
+
+    let username = parts[1];
+    let slug = parts[2];
+
+    Params {
+        username: username.into(),
+        slug: slug.into(),
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    let params = parse_params(cli.url);
+
+    println!("username: {:?}", params.username);
+    println!("slug: {:?}", params.slug);
 }
