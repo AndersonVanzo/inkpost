4dde3d901098e24e1c63623a47ef2926cabf9568
AndersonVanzo <anderson.vanzo01@gmail.com>
2026-09-23 02:17:57 -0300

    feat: improve url handling

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
 use clap::Parser;
 
 #[derive(Parser)]
-#[command(name = "inkpost")]
-#[command(version = "0.1.0")]
-#[command(about = "save DEV Community posts to your Kidle")]
+#[command(version, about)]
 struct Cli {
-    #[arg(long)]
+    /// URL from dev.to
     url: String,
 }
 
@@ -14,28 +12,50 @@ struct Params {
     slug: String,
 }
 
-fn parse_params(url: String) -> Params {
-    let normalized_url = url.replace("https://", "");
-    if !normalized_url.starts_with("dev.to/") {
-        panic!("URL must be from dev.to");
+fn parse_params(url: &str) -> Result<Params, String> {
+    let normalized_url = url.strip_prefix("https://").unwrap_or(url);
+    let normalized_url = normalized_url
+        .strip_prefix("http://")
+        .unwrap_or(normalized_url);
+
+    let url_params = normalized_url
+        .split(['#', '?'])
+        .next()
+        .unwrap_or(normalized_url);
+
+    let parts: Vec<&str> = url_params.split("/").collect();
+
+    let (username, slug) = match parts.as_slice() {
+        ["dev.to", first, second, ..] => (*first, *second),
+        [_, _, _, ..] => return Err("Invalid URL: must be from dev.to".into()),
+        _ => return Err("Invalid URL: not enough params".into()),
+    };
+
+    if username.is_empty() {
+        return Err("Invalid URL: username is empty".into());
     }
 
-    let parts: Vec<&str> = normalized_url.split("/").collect();
-
-    let username = parts[1];
-    let slug = parts[2];
-
-    Params {
-        username: username.into(),
-        slug: slug.into(),
+    if slug.is_empty() {
+        return Err("Invalid URL: slug is empty".into());
     }
+
+    Ok(Params {
+        username: username.to_owned(),
+        slug: slug.to_owned(),
+    })
 }
 
 fn main() {
     let cli = Cli::parse();
 
-    let params = parse_params(cli.url);
+    let params = match parse_params(&cli.url) {
+        Ok(params) => params,
+        Err(err) => {
+            eprintln!("{}", err);
+            std::process::exit(1);
+        }
+    };
 
-    println!("username: {:?}", params.username);
-    println!("slug: {:?}", params.slug);
+    println!("username: {}", params.username);
+    println!("slug: {}", params.slug);
 }
